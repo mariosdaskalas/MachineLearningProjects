539d6e2e8c18cb931a8a0b2cdfc85c442fb69442
Marios Daskalas <25797700+mariosdaskalas@users.noreply.github.com>
2026-08-05 17:35:05 +0300

    Update ml projects

---
+++ main.py
@@ -0,0 +1,2 @@
+print("Machine Learning Projects")
+print("This folder contains a collection of machine learning notebooks covering classification, regression, NLP, time series, and data analysis tasks.")
